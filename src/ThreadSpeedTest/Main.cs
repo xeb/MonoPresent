bb4671a8ea7606da3f0317106f16aa8371bfcc12
Mark Kockerbeck <mark@kockerbeck.com>
2012-01-17 23:42:52 -0800

    Main execution update

--- src/ThreadSpeedTest/Main.cs
+++ src/ThreadSpeedTest/Main.cs
@@ -21,36 +21,53 @@ namespace ThreadSpeedTest
 				numBatches = 10;
 			}
 			
+			bool synchronous = false;
+			
+			if(args != null && args.Length >= 2 && !bool.TryParse(args[1], out synchronous))
+			{
+				synchronous = false;
+			}
+			
 			// 100 batches
 			for(int batch = 1; batch <= numBatches; batch++)
 			{
 				_allTimes.AddRange(_batchTimes);
 				_batchTimes.Clear();
-				List<Task> tasks = new List<Task>();
 				
-				// Get Google 5 times
-				for(int i = 0; i < 5; i++)
+				if(synchronous)
 				{
-					tasks.Add(Task.Factory.StartNew(GetGoogle));
+					GetASite();
+				}
+				else
+				{
+					List<Task> tasks = new List<Task>();
+					
+					// Get Google 5 times
+					for(int i = 0; i < 5; i++)
+					{
+						tasks.Add(Task.Factory.StartNew(GetASite));
+					}
+					
+					Task.WaitAll(tasks.ToArray());
 				}
 				
-				Task.WaitAll(tasks.ToArray());
-					
-				Console.WriteLine("Batch {0}, Avg={1}, Min={2}, Max={3}", batch, _batchTimes.Average(), _batchTimes.Min(), _batchTimes.Max());
+				Console.WriteLine("{0} Batch {1}, Avg={2}, Min={3}, Max={4}", 
+				                  synchronous ? "Synchronous" : "Asynchronous",
+				                  batch, _batchTimes.Average(), _batchTimes.Min(), _batchTimes.Max());
 			}
 			
 			Console.WriteLine ("All, Avg={0}, Min={1}, Max={2}", _allTimes.Average(), _allTimes.Min(), _allTimes.Max());
 		}
 		
-		private static void GetGoogle()
+		private static void GetASite()
 		{
 			var sw = new Stopwatch();
 			sw.Start();
 			var wc = new WebClient();
+			wc.Headers.Add("user-agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0");
 			
 			// Assign to a variable just cause
-			var data = wc.DownloadString("http://www.google.com");
-			data.GetHashCode();
+			var data = wc.DownloadString("http://arcaneorb.com/");
 			
 			sw.Stop();
 			
